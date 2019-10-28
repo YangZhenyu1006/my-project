f8c098fd4f21dbd4ac97b2b64bf2c4b8a7ba4267
YangZhenyu <1332435292@qq.com>
2019-10-28 19:44:48 +0800

    主页部分

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,35 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '../views/login/index.vue'
+import Home from '../views/Home/index.vue'
+import welcome from '../views/welcome/index.vue'
+import NotFound from '../views/404/index.vue'
+
 Vue.use(VueRouter)
 const router = new VueRouter({
   // 路由配置对象
   // 关键选项：routes 作用：配置路由规则
   routes: [{
+    // 登录
     path: '/login',
     component: Login
-  }]
+  },
+  {
+    // 首页
+    path: '/',
+    component: Home,
+    children: [
+      // 欢迎
+      {
+        path: '/',
+        component: welcome
+      }
+    ]
+  },
+  {
+    path: '*',
+    component: NotFound
+  }
+  ]
 })
 export default router
---
+++ src/views/welcome/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <img src="../../assets/welcome.jpg" alt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped='less'>
+img {
+  display: block;
+  margin: 0 auto;
+}
+</style>
